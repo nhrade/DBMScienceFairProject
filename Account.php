5576a37390f1aa12c36185a6f420da2a6207a9d7
Noah Hradek <noahh717@gmail.com>
2017-10-26 16:46:25 -0600

    Changed account

--- Account.php
+++ Account.php
@@ -5,18 +5,20 @@
 
 class Account
 {
-    public $name, $email, $accountType;
+    public $name, $password, $email, $accountType;
 
 
     function  __construct($name, $email, $password, $accountType) {
         $this->name = $name;
         $this->email = $email;
+        $this->password;
         $this->accountType = $accountType;
     }
 
     function createAccountInDatabase() {
         $pass = password_hash($this->password, PASSWORD_BCRYPT);
-        $sql = "INSERT INTO login VALUES ($this->name, $this->email, $pass, $this->accountType)";
+        $login_table_name =  Config::LOGIN_TABLE_NAME;
+        $sql = "INSERT INTO  $login_table_name VALUES ($this->name, $this->email, $this->$pass, $this->accountType)";
         $db = mysqli_connect(Config::HOST, Config::UNAME,
             Config::PASSWORD, Config::DB_NAME) or die('Unable to connect to DB.');
         if($db->query($sql) === false) {
